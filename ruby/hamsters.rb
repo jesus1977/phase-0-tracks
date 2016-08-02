4eb0f8345b1e8130b2feca505f2ac77ccce88ea9
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-02 15:37:54 -0500

    final commits given feed back to clients

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -22,3 +22,21 @@ else
 	age = age.to_i
 end
 
+puts ""
+puts ""
+puts ""
+puts "Your hamster name is " + name 
+puts ""
+puts "The volume level of your hamster is " + volume_level.to_s
+puts ""
+puts "The color of your hamster is " + fur_color
+puts ""
+is_canidate = (is_canidate ? "yes" : "no") 
+puts "Is your hamster a good canidate for adoption? " + is_canidate 
+puts ""
+if age.nil? 
+   puts "You did not include age of your hamster."
+else
+    puts "Your hamster age is " + age.to_s
+end
+
