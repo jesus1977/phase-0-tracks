880cfd7cbcbf930db41ff9299f0b16e9e0c3c1f9
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-11 13:54:45 -0500

    printed psudeocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+#pseudocode
+
+	# ask each question
+	# get the reslut
+# get reslut convert it to interger
+# Ask  a boolean question
+# initial hash, or I guess I could have done client = {}
+# add values to keys from what the custoer input
+# Print the resluts
+# Ask to see if any changes needed to keys
+# In the reading it said to change the sym  is changing to symbol 
+# if else state to see if any needed changes. 
+# print the hash again, in its original  hash form
