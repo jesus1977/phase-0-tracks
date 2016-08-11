90dbadc0cd7c0abd6ed6368bf664b3cfc12fcef4
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-11 13:58:30 -0500

    Initialized hash

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,7 +1,7 @@
 #pseudocode
 
-	# ask each question
-	# get the reslut
+# ask each question
+# get the reslut(s)
 # get reslut convert it to interger
 # Ask  a boolean question
 # initial hash, or I guess I could have done client = {}
@@ -11,3 +11,49 @@
 # In the reading it said to change the sym  is changing to symbol 
 # if else state to see if any needed changes. 
 # print the hash again, in its original  hash form
+
+puts "We are now asking our clients what preferences you they like. please answer the following questions."
+
+	puts "What's your name"
+	# get the reslut(s)
+	name = gets.chomp
+
+	puts "What is your age?"
+	# get reslut convert it to interger
+	age = gets.chomp.to_i
+
+	puts "How many childeren do you have?"
+	number_of_children = gets.chomp.to_i
+
+	puts "What decor theme do you prefer"
+	decor_theme = gets.chomp
+	puts "What color do you prefer?"
+	color = gets.chomp
+
+	# Ask  a boolean question
+	puts "Do you like chandallers? (y/n)"
+	chandallers = gets.chomp
+		if chandallers == "y" || chandallers == "yes"
+			chandallers = true
+		else
+			chandallers = false
+		end
+
+	puts "Do you like night stands? (y/n)"
+	night_stand = gets.chomp
+		if night_stand == "y" || night_stand == "yes"
+			night_stand = true
+		else
+			night_stand = false
+		end
+# initial hash, or I guess I could have done client = {}
+	client = {
+		name: nil,
+		age: nil,
+		number_of_children: nil,
+		decor_theme: nil,
+		color: nil,
+		chandallers: nil,
+		nightstands: nil,
+
+	}
