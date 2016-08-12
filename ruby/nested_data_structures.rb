996fed6caca54717bed786a8d0323a809e2e142a
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-12 10:27:50 -0500

    Created scenariors for nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -29,3 +29,25 @@
 	}
 
 }
+
+puts "What sport(s) does Amy play?"
+puts class_room[:desk_two][:play_sport]
+puts " "
+puts "What's the pupils name who sits at desk one?"
+puts class_room[:desk_one][:alumni_name]
+puts " "
+puts "What the pupils name who sits at desk three"
+puts class_room[:desk_three][:alumni_name]
+puts " "
+puts "What sport does Abraham Addler play during the winter?"
+puts class_room[:desk_three][:play_sport][:winter_sport]
+puts " "
+puts "Reverse the school supplies for Amy"
+puts class_room[:desk_two][:supplies].reverse
+puts " "
+puts "Abraham Addler wants to join a baseball in the spring add it."
+puts class_room[:desk_three][:play_sport][:spring_sport] = "baseball"
+puts " "
+puts "Amy wants to borrow Abrahams calculator (take it from Abraham give it to Amy.)"
+puts class_room[:desk_three][:supplies].delete("calculator")
+puts class_room[:desk_two][:supplies] << 'calculator'
