f399b1aaae61c9f4813ccd33205dde13c51db5d6
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-12 10:26:54 -0500

    Created nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,31 @@
- 
+ class_room = {
+	desk_one: {
+		alumni_name: 'Jamie Smith',
+		supplies: ['pencils', 'cryaons', 'pens', 'paper'],
+		play_sport: {
+			fall_sport: ['Soccer'],
+			winter_sport: [],
+			spring_sport: ['baseball']
+		}
+	},
+	desk_two: {
+		alumni_name: 'Amy Hernandez',
+		supplies: ['glue', 'siccors', 'erasers', 'markers'],
+		play_sport: {
+			fall_sport: ['volley ball'],
+			winter_sport: [],
+			spring_sport: ['Track and Filed']
+		}
+	},
+	desk_three: {
+		alumni_name: 'Abraham Addler',
+		supplies: ['paper', 'calculator', 'ruler', 'stickers'],
+		play_sport: {
+			fall_sport: [],
+			winter_sport: ['Wrestling'],
+			spring_sport: []
+		}
+
+	}
+
+}
