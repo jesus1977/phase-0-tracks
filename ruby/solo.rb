14b08e6ca7b632464ba8223e20523ee5010e4eff
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-21 08:40:38 -0500

    Class added with attributes and methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -22,3 +22,49 @@
 #ask questions
 # display answers
 
+class Copy
+
+	attr_reader :name, :password, :copies, :scans
+	attr_accessor :paper_size, :color
+
+	def initialize(name, password)
+		@name = name
+		@password = password
+		@copies= 1
+		@color = "gray"
+		@size = "stanard"
+		@scans = []
+	end
+
+	def print_copy
+		@copies += 1
+		p "Printing #{@copies} copies"
+	end
+
+	def enter_color_copy
+		@color
+		p "Printing #{@color} color"
+	end
+
+	def paper_size
+		if @size == "stanard"
+			@size = "standard"
+		else
+			@size = "legal"
+		end
+	end
+
+	def store_scanned_pages
+		@scans 
+		@scans.each do |scan|
+		p "#{scanning} scanning page..."
+		end
+	end
+
+	def display_pages
+		p "Scanned pages"
+		@scans.each do |scan|
+			p "#{scan}"
+		end
+	end
+end
