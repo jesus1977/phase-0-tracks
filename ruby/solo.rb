4d84a0ee9eb798b330335b8b08e09b4b02d28e94
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-21 08:39:37 -0500

    Pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,24 @@
+#Psedocode
+#copier
+#attributes
+#________________
+# name (string)
+#userpassword (string)
+#copies needed (interger)
+#color or black copies (boolean)
+#size (string)
+# scan (array)
+
+#methods
+#_______________
+#enter number of copies (add one)
+#enter black or white 
+#enter paper size
+# store scanned images
+# display scanned images
+
+#user Interface
+#______________
+#ask questions
+# display answers
+
