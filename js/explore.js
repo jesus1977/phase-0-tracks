5b4ee0c338fa0830df9a35df2e7a5aa4e4153d24
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-23 10:59:49 -0500

    Created the function reverse_string

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,21 @@
 // Psedocode Release 4 
 
-// Make a function that takes in a word(s)
+// Make a function that takes in a word(s)(as a parameter)
 // and then reverses the word
 // Create an empty string within the function
 // Create a (for loop) that will iterate thru the words
 // starting at the end and work one by one backwards
 // Add each letter one by one into the empty string
 // return the string that was empty
+
+function reverse_string(string){
+
+	var empty_string = "";
+
+	for (var i = string.length -1; i >= 0; i--){
+
+		empty_string += string[i];
+	}
+	return empty_string
+}
+
