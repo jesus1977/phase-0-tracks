92f95ad0406ebb4feeaa0a07a5b70ac5e6f6b52c
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-08-23 10:52:25 -0500

    Initial commit along with a psedocode

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// Psedocode Release 4 
+
+// Make a function that takes in a word(s)
+// and then reverses the word
+// Create an empty string within the function
+// Create a (for loop) that will iterate thru the words
+// starting at the end and work one by one backwards
+// Add each letter one by one into the empty string
+// return the string that was empty
