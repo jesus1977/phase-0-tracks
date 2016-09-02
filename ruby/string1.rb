4908538aa1d37bae5bd7169cd2ccadd9f0dea0e6
Jesus Zambrano <jesuszmba@Jesuss-MacBook-Air.local>
2016-09-02 15:08:31 -0500

    Initial commit

---
+++ ruby/string1.rb
@@ -0,0 +1,41 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+
+ p "iNvEsTiGaTiOn".swapcase
+# => “InVeStIgAtIoN”
+
+  p "zom".insert(1, "o")
+# => “zoom”
+
+ "enhance".center(15)
+# => "    enhance    "
+
+ p "Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
+
+ p "the usual". << " suspects"
+#=> "the usual suspects"
+
+ p " suspects".prepend("the ")
+# => "the usual suspects"
+
+"The case of the disappearing last letter".slice(0..-2)
+# => "The case of the disappearing last lette"
+
+p "The mystery of the missing first letter".slice(1..-1)
+# => "he mystery of the missing first letter"
+
+ p "Elementary,    my   dear        Watson!".gsub(/\s+/, " ")
+# p "Helllllllo".gsub(/l+/, "ll") (just testing it out)
+# => "Elementary, my dear Watson!"
+
+p "z".ord
+# => 122 
+# (What is the significance of the number 122 in relation to the character z?)
+p "It is the value of ASCII printable characters"
+
+p "How many times does the letter 'a' appear in this string?".count('a')
+# => 4
