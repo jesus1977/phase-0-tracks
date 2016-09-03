fa7cc638cd5a92b2fd2a51665fc21b51649dbee4
Jesus Zambrano <jesuszambrano77@yahoo.com>
2016-09-03 13:49:07 -0500

    Added coded

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,1 +1,73 @@
+# 
 
+client_hash = {}
+
+puts "We are going to ask some questions about home design and other questions"
+
+puts "Whats your name?"
+name = gets.chomp
+client_hash[:name] = name.to_s
+
+puts "How old are you?"
+age = gets.chomp
+client_hash[:age] = age.to_i
+
+puts "How many childern do you have?"
+childern = gets.chomp
+client_hash[:childern] = childern.to_i
+
+puts "What kind of decor theme do you like? "
+decor_thme = gets.chomp
+client_hash[:decortheme] = decor_thme.to_s
+
+puts "Do you like wall paper?"
+wall_paper = gets.chomp
+if wall_paper.upcase == "YES"
+	wall_paper = true
+else
+	wall_paper = false
+end
+
+client_hash[:wall_paper] = wall_paper.to_s
+
+puts "Do you like carpet?"
+carpet = gets.chomp
+if carpet.downcase == "no"
+	carpet = false
+else
+	carpet = true
+end
+
+client_hash[:carpet] = carpet.to_s
+
+puts "Here are your selections"
+
+
+def print_hash(hash)
+	puts
+	for i in 0...hash.length
+		puts (hash.keys[i].to_s.capitalize + ":" + hash.values[i].to_s)
+	end
+end
+
+print_hash(client_hash)
+
+
+puts "Would you like to update any infromation now, just type in which value you want to update for example (name, age, etc.) or type exit to finhish with no new updates."
+
+new_hash = {}
+loop do 
+	puts "Enter an item or 'exit'."
+	input = gets.chomp
+  if input == 'exit'
+  	break
+  else
+	input.to_sym
+	puts "Now enter the value you want to chang it to"
+	value = gets.chomp
+	new_hash << client_hash[input] = value
+  end
+end
+
+puts "Here is your new updated list"
+print_hash(new_hash)
